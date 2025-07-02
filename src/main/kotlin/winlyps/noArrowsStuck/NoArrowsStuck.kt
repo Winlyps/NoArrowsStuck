833faece2a9600f06fc350b0f88304fc908bd947
Winlypse <nts3@o2.pl>
2025-07-02 21:30:40 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noArrowsStuck/NoArrowsStuck.kt
@@ -0,0 +1,14 @@
+package winlyps.noArrowsStuck
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoArrowsStuck : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
