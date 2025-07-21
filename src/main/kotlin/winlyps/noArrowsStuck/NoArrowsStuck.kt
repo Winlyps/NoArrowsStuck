4adc0babeeb98604299f1b6fbbaa430e6cceada3
Winlypse <nts3@o2.pl>
2025-07-21 10:39:14 +0200

    code!

--- src/main/kotlin/winlyps/noArrowsStuck/NoArrowsStuck.kt
+++ src/main/kotlin/winlyps/noArrowsStuck/NoArrowsStuck.kt
@@ -1,14 +1,29 @@
 package winlyps.noArrowsStuck
 
+import org.bukkit.entity.Arrow
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityDamageByEntityEvent
 import org.bukkit.plugin.java.JavaPlugin
 
-class NoArrowsStuck : JavaPlugin() {
+class NoArrowsStuck : JavaPlugin(), Listener {
 
     override fun onEnable() {
-        // Plugin startup logic
+        server.pluginManager.registerEvents(this, this)
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+    }
+
+    @EventHandler
+    fun onArrowHit(event: EntityDamageByEntityEvent) {
+        if (event.entity is Player && event.damager is Arrow) {
+            val player = event.entity as Player
+            server.scheduler.runTaskLater(this, Runnable {
+                player.arrowsInBody = 0
+            }, 1L)
+        }
     }
 }
